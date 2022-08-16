50340563249868be4ced047cac9b674fd18f1595
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-08-16 14:33:05 +0300

    Update example2.java

--- example2.java
+++ example2.java
@@ -1,4 +1,14 @@
-   private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+/*
+reading / retrieving JSON response value via HttpRequest
+note: details of where this code is in use is not shared due to me and my partners privacy and copyright reasons. 
+Currently, we will not share the complete code as we don't want it to be open-source yet.
+note 2: code has been altered to not be the exact copy of the version I made professionally, and can not be recognized.
+note 3: code is missing essential parts that makes it function correctly (for me and my partners privacy reasons as well).
+note 4: All the rights of this particular code belong to me, this spesific code can be used freely, with credits pointing to this page.
+*/  
+
+
+private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         
         final String SOME_URL = jTextField1.getText();
         final String SOME_VALUE_IDENTIFIER = jTextField3.getText();
